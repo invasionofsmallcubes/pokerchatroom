a3fa9218f0b26f5b6931f230a524d7fc6ca79e65
Emanuele Ianni <emanuele.ianni@maersk.com>
2020-04-13 16:50:03 +0200

    fixed prettier

--- nextState.test.js
+++ nextState.test.js
@@ -3,17 +3,16 @@ const NextState = require('./nextState')
 function Chat() {
   return {
     // eslint-disable-next-line no-unused-vars
-    game: jest.fn((roomName, message) => { }),
+    game: jest.fn((roomName, message) => {}),
     // eslint-disable-next-line no-unused-vars
-    toRoomInTopic: jest.fn((room, message, topic) => { }),
+    toRoomInTopic: jest.fn((room, message, topic) => {}),
   }
 }
 
 function NextStateMock() {
   return {
     // eslint-disable-next-line no-unused-vars
-    print: jest.fn((chat) => { }),
-
+    print: jest.fn((chat) => {}),
   }
 }
 
